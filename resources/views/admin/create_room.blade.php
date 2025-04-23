ab8d6f142cf240ed47df3f0f22599111e98fa1ce
Ashan <ashanvimodh241@gmail.com>
2025-04-23 22:44:52 +0530

    12.46

--- resources/views/admin/create_room.blade.php
+++ resources/views/admin/create_room.blade.php
@@ -10,6 +10,39 @@
 
     <!-- Sidebar Navigation-->
     @include('admin.sidebar')
+
+    <div class="page-content">
+        <div class="page-header">
+            <div class="container-fluid">
+                <div>
+                    <form>
+                        <div>
+                            <label>Room Title</label>
+                            <input type="text" name="title">
+                        </div>
+
+                        <div>
+                            <label>Description</label>
+                            <textarea name="description"></textarea>
+                        </div>
+
+                        <div>
+                            <label>Price</label>
+                            <input type="number" name="price">
+
+                            <div>
+                            <label>Room Type</label>
+                            <select>
+                                
+                            </select>
+                        </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- foter section -->
     @include('admin.footer')
 </body>
